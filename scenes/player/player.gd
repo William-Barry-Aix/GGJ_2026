e5dc3858c91701d0c9b80662b806052b32c02f90
Felix <f.a.r.barrette@hotmail.com>
2026-01-30 22:13:49 -0500

    Felix test scene added. Debug features added to root and player

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -3,6 +3,8 @@ class_name Player
 
 @export var move_speed: float = 300.0
 
+signal reset_mob_target_pos(Vector2)
+
 func _physics_process(delta: float) -> void:
 	_handle_movement(delta)
 
@@ -12,5 +14,7 @@ func _handle_movement(delta: float) -> void:
 		Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
 	).normalized()
 	
+	if input != Vector2.ZERO:
+		emit_signal("reset_mob_target_pos", position)
 	velocity = input * move_speed
 	move_and_slide()
